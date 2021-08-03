1ec97fd61071d0be73e76001fb243a153c7141c9
Tzong-Hann Wu <tzonghann.wu@gmail.com>
2021-08-03 20:58:03 +0800

    replace grpc to gRPCConfig

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import defaultRouter from './routes/default.js'
 import healthRouter from './routes/health.js'
-import grpc from '@grpc/grpc-js'
+// import grpc from '@grpc/grpc-js'
 import pingServices from './grpc/services/ping.js'
 import { URL } from 'url'
 import path from 'path'
@@ -19,10 +19,10 @@ httpServer.listen(httpPort, () => {
 const __dirname = new URL('.', import.meta.url).pathname
 const PROTO_PATH = path.join(__dirname + '/grpc/protos/ping.proto')
 const protoDescriptor = gRPCConfig.GetProtoDescriptor(PROTO_PATH)
-const gRPCServer = new grpc.Server()
+const gRPCServer = new gRPCConfig.grpc.Server()
 const gRPCPort = 8081
 gRPCServer.addService(protoDescriptor.Ping.service, { Echo: pingServices.Echo, Testing: pingServices.Testing })
-gRPCServer.bindAsync(`0.0.0.0:${gRPCPort}`, grpc.ServerCredentials.createInsecure(), () => {
+gRPCServer.bindAsync(`0.0.0.0:${gRPCPort}`, gRPCConfig.grpc.ServerCredentials.createInsecure(), () => {
   gRPCServer.start()
   console.log(`gRPC Server listening at http://localhost:${gRPCPort}`)
 })
