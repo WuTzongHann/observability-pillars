fe0e24bbb4e09e78856c697b4248183d1b122051
Tzong-Hann Wu <tzonghann.wu@gmail.com>
2021-08-11 11:40:15 +0800

    add http 405 handler

--- nodejsserver/routes/default.js
+++ nodejsserver/routes/default.js
@@ -1,10 +1,19 @@
 import express from 'express'
-
 import defaultController from '../controllers/default.js'
 
+const notAllowedMethod = (req, res) => {
+  res.status(405).send()
+}
 const router = express.Router()
-router.get('/', defaultController.sayHelloWorld)
-router.get('/health', defaultController.responseHealth)
-router.post('/echo', defaultController.echoYourRequest)
+router.all((req, res) => {
+  res.status(405).send()
+})
+router.route('/')
+  .get(defaultController.sayHelloWorld)
+router.route('/health')
+  .get(defaultController.responseHealth)
+router.route('/echo')
+  .post(defaultController.echoYourRequest)
+  .all(notAllowedMethod)
 
 export default router
--- nodejsserver/routes/health.js
+++ nodejsserver/routes/health.js
@@ -1,8 +1,12 @@
 import express from 'express'
-
 import healthController from '../controllers/health.js'
 
+const notAllowedMethod = (req, res) => {
+  res.status(405).send()
+}
 const router = express.Router()
-router.get('/', healthController.responseHealth)
+router.route('/')
+  .get(healthController.responseHealth)
+  .all(notAllowedMethod)
 
 export default router
