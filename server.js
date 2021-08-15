6600cf805ce1443871c3c0dc2ccaad5e1eca8862
Tzong-Hann Wu <tzonghann.wu@gmail.com>
2021-08-15 15:07:35 +0800

    add metrics to grpc

--- server.js
+++ server.js
@@ -1,50 +1,28 @@
 import express from 'express'
-import grpc from '@grpc/grpc-js'
-import protoLoader from '@grpc/proto-loader'
-
+import Mali from 'mali'
 import defaultRouter from './http/routes/default.js'
 import patientsRouter from './http/routes/patients.js'
-
-import pingService from './grpc/services/ping.js'
-
-import metricsMiddleware from './http/middlewares/metrics.js'
+import {
+  httpMetricsMiddleware,
+  grpcMetricsInterceptor
+} from './metrics/index.js'
+import {
+  unsupportedMediaTypeHandler,
+  notFoundHandler,
+  errorHandler
+} from './http/handlers/index.js'
+import {
+  echo,
+  testing
+} from './grpc/services/ping.js'
 
 const HTTP_PORT = 8080
 const GRPC_PORT = 8081
-const PingProtoPath = './grpc/protos/ping.proto'
+const PROTO_PATH = './grpc/protos/ping.proto'
 
-const loadProtoDescriptor = protoPath => {
-  const packageDefinition = protoLoader.loadSync(
-    protoPath,
-    {
-      keepCase: true,
-      longs: String,
-      enums: String,
-      defaults: true,
-      oneofs: true
-    })
-  const protoDescriptor = grpc.loadPackageDefinition(packageDefinition)
-  return protoDescriptor
-}
-
-// Handlers
-const unsupportedMediaTypeHandler = (req, res, next) => {
-  if (!req.is('application/json')) {
-    res.status(415).send('Your content-type is not "application/json" or You did not provide any data.')
-    return
-  }
-  next()
-}
-const notFoundHandler = (req, res) => {
-  res.status(404).send('Sorry can\'t find that!')
-}
-const errorHandler = (err, req, res, next) => {
-  res.status((err.statusCode === undefined) ? 500 : err.statusCode).json({ error: err })
-}
-
-const main = () => {
+const main = async () => {
   const httpServer = express()
-  httpServer.use(metricsMiddleware())
+  httpServer.use(httpMetricsMiddleware())
   httpServer.use(unsupportedMediaTypeHandler)
   httpServer.use(express.json())
   httpServer.use(defaultRouter)
@@ -55,13 +33,11 @@ const main = () => {
     console.log(`HTTP Server listening at http://localhost:${HTTP_PORT}`)
   })
 
-  const pingDescriptor = loadProtoDescriptor(PingProtoPath)
-  const gRPCServer = new grpc.Server()
-  gRPCServer.addService(pingDescriptor.Ping.service, { Echo: pingService.Echo, Testing: pingService.Testing })
-  gRPCServer.bindAsync(`0.0.0.0:${GRPC_PORT}`, grpc.ServerCredentials.createInsecure(), () => {
-    gRPCServer.start()
-    console.log(`gRPC Server listening at http://localhost:${GRPC_PORT}`)
-  })
+  const gRPCServer = new Mali(PROTO_PATH)
+  gRPCServer.use(grpcMetricsInterceptor)
+  gRPCServer.use({ echo, testing })
+  await gRPCServer.start(`0.0.0.0:${GRPC_PORT}`)
+  console.log(`gRPC Server listening at http://localhost:${GRPC_PORT}`)
 }
 
 main()
