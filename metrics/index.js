15b3d741468996443398e570e3cdf2d3f6af19ab
Tzong-Hann Wu <tzonghann.wu@gmail.com>
2021-08-15 15:08:41 +0800

    use collectDefaultMetrics() as default

--- metrics/index.js
+++ metrics/index.js
@@ -1,5 +1,8 @@
 import httpMetricsMiddleware from './httpMetrics.js'
 import grpcMetricsInterceptor from './grpcMetrics.js'
 
+import prometheus from 'prom-client'
+prometheus.collectDefaultMetrics()
+
 export default { httpMetricsMiddleware, grpcMetricsInterceptor }
 export { httpMetricsMiddleware, grpcMetricsInterceptor }
