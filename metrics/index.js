cfc79cd55beaacd2559748703687d172a5e00042
Tzong-Hann Wu <tzonghann.wu@gmail.com>
2021-08-15 11:55:04 +0800

    manage http metrics and grpc metrics together

---
+++ metrics/index.js
@@ -0,0 +1,5 @@
+import httpMetricsMiddleware from './httpMetrics.js'
+import grpcMetricsInterceptor from './grpcMetrics.js'
+
+export default { httpMetricsMiddleware, grpcMetricsInterceptor }
+export { httpMetricsMiddleware, grpcMetricsInterceptor }
