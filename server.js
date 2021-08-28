5df8dad4462e2fe6b8f4a82ae239b68fdfcdd9af
Tzong-Hann Wu <tzonghann.wu@gmail.com>
2021-08-28 21:45:36 +0800

    update logger and add gotoHTTP and gotoGRPC method

--- server.js
+++ server.js
@@ -3,16 +3,14 @@ import Mali from 'mali'
 import defaultRouter from './http/routes/default.js'
 import patientsRouter from './http/routes/patients.js'
 import Metrics from './metrics/index.js'
-import tracing from './traces/index.js'
 import Logger from './logs/index.js'
-import {
-  unsupportedMediaTypeHandler,
-  notFoundHandler,
-  errorHandler
-} from './http/handlers/index.js'
+import traces from './traces/index.js'
+import handlers from './http/handlers/index.js'
 import {
   echo,
-  testing
+  testing,
+  gotoHTTP/* ,
+  gotoGRPC */
 } from './grpc/services/ping.js'
 
 const HTTP_PORT = 8080
@@ -24,21 +22,22 @@ const main = async () => {
   const logger = new Logger()
 
   const httpServer = express()
-  httpServer.use(tracing(logger))
+  httpServer.use(traces.httpMiddleware(logger))
   httpServer.use(metrics.httpMiddleware())
-  httpServer.use(unsupportedMediaTypeHandler)
   httpServer.use(express.json())
+  httpServer.use(handlers.unsupportedMediaType)
   httpServer.use(defaultRouter)
   httpServer.use('/patients', patientsRouter)
-  httpServer.use(notFoundHandler)
-  httpServer.use(errorHandler)
+  httpServer.use(handlers.notFound)
+  httpServer.use(handlers.error)
   httpServer.listen(HTTP_PORT, () => {
     logger.info(`HTTP Server listening at http://localhost:${HTTP_PORT}`)
   })
 
   const gRPCServer = new Mali(PROTO_PATH)
+  gRPCServer.use(traces.grpcInterceptor(logger))
   gRPCServer.use(metrics.grpcInterceptor())
-  gRPCServer.use({ echo, testing })
+  gRPCServer.use({ echo, testing, gotoHTTP/* , gotoGRPC */ })
   await gRPCServer.start(`0.0.0.0:${GRPC_PORT}`)
   logger.info(`gRPC Server listening at http://localhost:${GRPC_PORT}`)
 }
