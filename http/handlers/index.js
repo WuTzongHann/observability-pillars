6cf20f8e4bc9998efa08ecb90752c879a88c203b
Tzong-Hann Wu <tzonghann.wu@gmail.com>
2021-08-28 21:37:20 +0800

    update names of handlers

--- http/handlers/index.js
+++ http/handlers/index.js
@@ -1,21 +1,21 @@
-const unsupportedMediaTypeHandler = (req, res, next) => {
+const unsupportedMediaType = async (req, res, next) => {
   if (!req.is('application/json')) {
     res.status(415).send('Your content-type is not "application/json" or You did not provide any data.')
     const { originalUrl: urlPath, method } = req
     res.locals.logger.error('Your content-type is not "application/json" or You did not provide any data.', { urlPath, method, statusCode: res.statusCode })
     return
   }
-  next()
+  await next()
 }
-const notFoundHandler = (req, res) => {
+const notFound = (req, res) => {
   res.status(404).send('Sorry can\'t find that!')
   const { originalUrl: urlPath, method } = req
   res.locals.logger.error('Sorry can\'t find that!', { urlPath, method, statusCode: res.statusCode })
 }
-const errorHandler = (err, req, res, next) => {
+const error = (err, req, res, next) => {
   res.status((err.statusCode === undefined) ? 500 : err.statusCode).json({ error: err })
   const { originalUrl: urlPath, method } = req
   res.locals.logger.error(err, { urlPath, method, statusCode: res.statusCode })
 }
 
-export { unsupportedMediaTypeHandler, notFoundHandler, errorHandler }
+export default { unsupportedMediaType, notFound, error }
