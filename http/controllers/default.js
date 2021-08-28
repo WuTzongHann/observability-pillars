2aee720e14fb990f3565c9a94bcf59fcd76d3cf0
Tzong-Hann Wu <tzonghann.wu@gmail.com>
2021-08-28 21:33:06 +0800

    add gotoHTTP and gotoGRPC method

--- http/controllers/default.js
+++ http/controllers/default.js
@@ -1,3 +1,8 @@
+import axios from 'axios'
+import path from 'path'
+import GRPCClient from 'node-grpc-client'
+import traces from '../../traces/index.js'
+
 const sayHelloWorld = (req, res) => {
   res.send('Hello World!')
   const { originalUrl: urlPath, method } = req
@@ -26,4 +31,61 @@ const echoYourRequest = (req, res) => {
   res.locals.logger.info('User Visited', { urlPath, method, statusCode: res.statusCode })
 }
 
-export default { sayHelloWorld, responseHealth, echoYourRequest }
+const gotoHTTP = async (req, res) => {
+  const newHeaders = { 'content-type': 'application/json' }
+  traces.SetOutgoingHeaderWithTraceFromHeader(req.headers, newHeaders)
+  await axios.get('http://localhost:8080/health',
+    {
+      headers: newHeaders,
+      data: {
+        message_id: 'qwert',
+        message_body: 'hello ping service'
+      }
+    })
+    .then(function (response) {
+      res.send(response.data)
+      const { originalUrl: urlPath, method } = req
+      res.locals.logger.info('User Visited', { urlPath, method, statusCode: res.statusCode })
+    })
+}
+//still need to use async/await
+const gotoGRPC = (req, res) => {
+  const PROTO_PATH = path.resolve('./grpc/protos/ping.proto')
+  const myClient = new GRPCClient(PROTO_PATH, 'myPing', 'Ping', 'localhost:8081')
+  const options = { metadata: {} }
+  options.metadata = traces.NewOutgoingContextWithTraceFromHeader(req.headers)
+
+  myClient.runService('echo', { message_id: 'qwert', message_body: 'hello ping service' }, (err, response) => {
+    if (err) {
+      console.log(err)
+      response.json(err)
+      return
+    }
+    console.log('Service response ', response)
+    res.json(response)
+  }, options)
+}
+
+const returnAnError = () => {
+  throw new Error('I am error!')
+}
+
+const responseError = (req, res) => {
+  returnAnError()
+  res.status(500).json('error occurred')
+}
+
+const waitMilliSeconds = (ms) => {
+  const start = new Date().getTime()
+  let end = start
+  while (end < start + ms) {
+    end = new Date().getTime()
+  }
+}
+
+const responseAsync = async (req, res) => {
+  await waitMilliSeconds(3000)
+  res.status(200).json('async completed')
+}
+
+export default { sayHelloWorld, responseHealth, echoYourRequest, gotoHTTP, gotoGRPC, responseError, responseAsync }
