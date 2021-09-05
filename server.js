bb4cf9fdcddc56354bab9b9c8fadd13e3c1783c8
Tzong-Hann Wu <tzonghann.wu@gmail.com>
2021-09-05 17:31:50 +0800

    use httpServer and grpcServer from pillars

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 import express from 'express'
-import Mali from 'mali'
-import morgan from 'morgan'
-import defaultRouter from './http/routes/default.js'
-import patientsRouter from './http/routes/patients.js'
-import Metrics from './metrics/index.js'
-import Logger from './logs/index.js'
-import traces from './traces/index.js'
-import handlers from './http/handlers/index.js'
-import pingMethods from './grpc/services/ping.js'
+import Pillars from './index.js'
+import defaultRouter from './example/http/routes/default.js'
+import handlers from './example/http/handlers/index.js'
+import pingMethods from './example/grpc/services/ping.js'
 
 const {
   echo,
@@ -18,40 +13,23 @@ const {
 
 const HTTP_PORT = 8080
 const GRPC_PORT = 8081
-const PROTO_PATH = './grpc/protos/ping.proto'
 
 const main = async () => {
-  const metrics = new Metrics()
-  const logger = new Logger()
+  const pillars = new Pillars({ PROTO_PATH: './example/grpc/protos/ping.proto' })
 
-  const httpServer = express()
-  httpServer.use(traces.httpMiddleware(logger))
-  httpServer.use(metrics.httpMiddleware())
-  httpServer.use(morgan(function (tokens, req, res) {
-    return JSON.stringify({
-      method: tokens.method(req, res),
-      url: tokens.url(req, res),
-      status: tokens.status(req, res),
-      'content-length': tokens.res(req, res, 'content-length'),
-      'response-time': tokens['response-time'](req, res)
-    })
-  }, { stream: logger.stream }))
+  const httpServer = pillars.httpServer
   httpServer.use(express.json())
   httpServer.use(handlers.unsupportedMediaType)
   httpServer.use(defaultRouter)
-  httpServer.use('/patients', patientsRouter)
   httpServer.use(handlers.notFound)
   httpServer.use(handlers.error)
   httpServer.listen(HTTP_PORT, () => {
-    logger.info(`HTTP Server listening at http://localhost:${HTTP_PORT}`)
+    pillars.logger.info(`HTTP Server listening at http://localhost:${HTTP_PORT}`)
   })
-
-  const gRPCServer = new Mali(PROTO_PATH)
-  gRPCServer.use(traces.grpcInterceptor(logger))
-  gRPCServer.use(metrics.grpcInterceptor())
-  gRPCServer.use({ echo, testing, gotoHTTP, gotoGRPC })
-  await gRPCServer.start(`0.0.0.0:${GRPC_PORT}`)
-  logger.info(`gRPC Server listening at http://localhost:${GRPC_PORT}`)
+  const grpcServer = pillars.grpcServer
+  grpcServer.use({ echo, testing, gotoHTTP, gotoGRPC })
+  await grpcServer.start(`0.0.0.0:${GRPC_PORT}`)
+  pillars.logger.info(`grpc Server listening at http://localhost:${GRPC_PORT}`)
 }
 
 main()
