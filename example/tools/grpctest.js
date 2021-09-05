eb109922ef827556e5bc0e718f41d39455af57f3
Tzong-Hann Wu <tzonghann.wu@gmail.com>
2021-09-05 19:30:58 +0800

    update sendRequest func of http and grpc

---
+++ example/tools/grpctest.js
@@ -0,0 +1,25 @@
+import path from 'path'
+import GRPCClient from 'node-grpc-client'
+
+const PROTO_PATH = path.resolve('../grpc/protos/ping.proto')
+const myClient = new GRPCClient(PROTO_PATH, 'myPing', 'Ping', 'localhost:8081')
+const options = { metadata: {} }
+options.metadata = {
+  'x-request-id': 'exampleXRequestId',
+  'x-b3-traceid': 'exampleXB3TraceId',
+  'x-b3-spanid': 'exampleXB3SpanId',
+  'x-b3-parentspanid': 'exampleXB3ParentId'
+}
+
+function main (args) {
+  const method = (args.length > 0) ? args[0] : 'health'
+  myClient.runService(method, { message_id: 'exampleId', message_body: 'exampleBody' }, (err, res) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+    console.log('Service response ', res)
+  }, options)
+}
+
+main(process.argv.slice(2))
