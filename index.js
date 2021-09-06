c48c219a31c147020a499b61248924413e9b5441
maxcian <maxcian@jubo.health>
2021-09-06 10:09:56 +0800

    refactor: update

--- index.js
+++ index.js
@@ -12,10 +12,17 @@ const defaultOptions = {
 morgan.token('trace_id', (req, res) => res.locals.logger.trace_id)
 morgan.token('span_id', (req, res) => res.locals.logger.span_id)
 
+// NOTE: package scope
+// application server 通常不會同時監聽 gRPC 與 HTTP 作為應用
+// 結果你的 package 目前的設計，卻會替 application server 加了一些它不需要的東西
+// 須重新思考你的 package scope，思考如何提供足夠的需求，小心陷入「過早最佳化」的泥淖
 class Pillars {
   constructor (userOptions = {}) {
     const options = { ...defaultOptions, ...userOptions }
     const { PROTO_PATH } = options
+    // NOTE: style not consistency
+    // 為何 Metrics 與 Logger 是物件、traces 卻是 pure function？用法是否可以統一？
+    // 還是你覺得有什麼是 package maintainer 需要特別注意的，所以用不同的風格呢？
     const metrics = new Metrics()
     const logger = new Logger()
     const httpServer = express()
@@ -30,7 +37,7 @@ class Pillars {
         urlPath: tokens.url(req, res)
       })
     }, { stream: logger.stream }))
-
+    // NOTE: red flag: if PROTO_PATH is not given? what happen?
     const grpcServer = new Mali(PROTO_PATH)
     grpcServer.use(traces.grpcInterceptor(logger))
     grpcServer.use(metrics.grpcInterceptor())
